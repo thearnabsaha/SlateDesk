c57da461adfd73ff610f439ddf6df8f0a7bb30ef
thearnabsaha <thearnabsaha201@gmail.com>
2025-07-29 22:53:52 +0530

    feat: add resizable components to layout and update sidebar trigger styling

--- apps/web/app/dashboard/layout.tsx
+++ apps/web/app/dashboard/layout.tsx
@@ -1,12 +1,16 @@
 import { SidebarProvider, SidebarTrigger } from "@workspace/ui/components/sidebar"
 import { AppSidebar } from "../../components/app-sidebar"
-
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@workspace/ui/components/resizable"
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <SidebarProvider>
             <AppSidebar />
             <main>
-                <SidebarTrigger />
+                <SidebarTrigger/>
                 {children}
             </main>
         </SidebarProvider>
--- apps/web/components/app-sidebar.tsx
+++ apps/web/components/app-sidebar.tsx
@@ -1,20 +1,20 @@
 import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarGroup,
-  SidebarHeader,
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarHeader,
 } from "@workspace/ui/components/sidebar"
 
 export function AppSidebar() {
-  return (
-    <Sidebar>
-      <SidebarHeader />
-      <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
-      </SidebarContent>
-      <SidebarFooter />
-    </Sidebar>
-  )
+    return (
+        <Sidebar>
+            <SidebarHeader />
+            <SidebarContent>
+                <SidebarGroup />
+                <SidebarGroup />
+            </SidebarContent>
+            <SidebarFooter />
+        </Sidebar>
+    )
 }
