89aba74e9fe57ac58f323ab2f65fd47be524a9b8
thearnabsaha <thearnabsaha201@gmail.com>
2025-07-29 15:40:50 +0530

    style: update layout and styling of the main page and mode toggle component

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -12,15 +12,15 @@ import { cn } from "@workspace/ui/lib/utils"
 const page = () => {
   const scrolled= useScrollTop()
   return (
-    <div>
-      <div className={cn("flex justify-between items-center px-10 py-2 z-50 fixed w-full top-0 bg-background",scrolled&&" border-b shadow-sm shadow-ring")}>
-        <h1>SlateDesk</h1>
+    <div className="flex flex-col min-h-screen">
+      <div className={cn("flex justify-between items-center px-6 py-4 z-50 fixed w-full top-0 bg-background flex-1",scrolled&&" border-b shadow-sm shadow-ring")}>
+        <h1 className="text-3xl">SlateDesk</h1>
         <div className="flex space-x-2">
-          <Button>Login</Button>
+          <Button className="cursor-pointer">Login</Button>
           <ModeToggle/>
         </div>
       </div>
-      <div className="flex flex-col pt-30 items-center">
+      <div className="flex flex-col pt-30 items-center flex-1">
         <h1 className=" text-4xl w-full md:w-[500px] text-center px-5">Your Ideas, Documents, & Plans. Unified. Welcome to <span className=" underline">SlateDesk</span> </h1>
         <p className=" text-center w-full md:w-[400px] mt-10 px-5">Capture notes, plan projects, and build your second brain—all in one clean, customizable space.</p>
         <div className="flex mt-10 md:mt-0 h-[500px]">
@@ -28,7 +28,7 @@ const page = () => {
           <Image src={b} alt="book" className="w-[300px] m-30 lg:block hidden"/>
         </div>
       </div>
-      <footer className="flex justify-between px-5 relative bottom-0 ">
+      <footer className="flex justify-between px-5 relative w-full mb-2 bottom-0 ">
         <p className="text-lg font-black">SlateDesk</p>
         <div className="text-muted-foreground flex space-x-5 flex-col sm:flex-row">
           <p>Privacy Policy</p>
--- apps/web/components/ModeToggle.tsx
+++ apps/web/components/ModeToggle.tsx
@@ -18,20 +18,20 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+        <Button variant="outline" size="icon" className="border-ring border cursor-pointer">
           <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")} className="px-5">
+        <DropdownMenuItem onClick={() => setTheme("light")} className="px-5 cursor-pointer">
           Light
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")} className="px-5">
+        <DropdownMenuItem onClick={() => setTheme("dark")} className="px-5 cursor-pointer">
           Dark
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")} className="px-5">
+        <DropdownMenuItem onClick={() => setTheme("system")} className="px-5 cursor-pointer">
           System
         </DropdownMenuItem>
       </DropdownMenuContent>
