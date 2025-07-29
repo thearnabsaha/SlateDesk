53496c69e9eab3a65d4a3b3a52c73fb500bbf5ef
thearnabsaha <thearnabsaha201@gmail.com>
2025-07-29 22:48:00 +0530

    feat: add new Radix UI components and react-resizable-panels as dependencies

---
+++ apps/web/app/dashboard/layout.tsx
@@ -0,0 +1,14 @@
+import { SidebarProvider, SidebarTrigger } from "@workspace/ui/components/sidebar"
+import { AppSidebar } from "../../components/app-sidebar"
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <SidebarProvider>
+            <AppSidebar />
+            <main>
+                <SidebarTrigger />
+                {children}
+            </main>
+        </SidebarProvider>
+    )
+}
