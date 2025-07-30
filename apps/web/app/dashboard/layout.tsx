221a0e8e83987a3d5e9456c429bf1a55e1a55dd3
thearnabsaha <thearnabsaha201@gmail.com>
2025-07-30 22:20:53 +0530

    feat: enhance dashboard layout and user session handling with logout functionality

--- apps/web/app/dashboard/layout.tsx
+++ apps/web/app/dashboard/layout.tsx
@@ -1,15 +1,15 @@
 import { SidebarProvider, SidebarTrigger } from "@workspace/ui/components/sidebar"
 import { AppSidebar } from "../../components/app-sidebar"
 import {
-  ResizableHandle,
-  ResizablePanel,
-  ResizablePanelGroup,
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup,
 } from "@workspace/ui/components/resizable"
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <SidebarProvider>
             <AppSidebar />
-            <main>
+            <main className="flex flex-col w-full">
                 <SidebarTrigger />
                 {children}
             </main>
--- apps/web/app/dashboard/page.tsx
+++ apps/web/app/dashboard/page.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
-
+"use client"
+import { signOut, useSession } from "next-auth/react"
+import React, { useEffect } from 'react'
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import c from '../../public/c.svg'
+import { Button } from "@workspace/ui/components/button";
+import { CirclePlus, Plus } from "lucide-react";
 const Dashboard = () => {
+  const router = useRouter()
+  const { data: session, status } = useSession()
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/")
+    }
+    if (status === "loading") {
+      <h1>Loading....</h1>
+    }
+  }, [])
+
   return (
-    <div>
-        
+    <div className="flex flex-col justify-center w-full h-full">
+      {/* <h1>Dashboard</h1> */}
+      <button
+        className="btn btn-secondary"
+        onClick={() => signOut({ callbackUrl: "/" })}
+      >
+        Logout
+      </button>
+      {
+        session && <div className="flex flex-col justify-center items-center w-full">
+          {/* <Image src={c} alt="book" className="w-[300px] m-10" /> */}
+          <Image src={c} alt="book" className="sm:w-[300px] w-[200px] m-10" />
+          <h1>Welcome to {session.user?.name?.split(" ")[0]}'s Desk</h1>
+          <Button className="mt-2"><CirclePlus /> Create Slate</Button>
+        </div>
+      }
+
+
     </div>
   )
 }
