ef7ed04aafb28baf8d8b35b88c537be80bb5d064
thearnabsaha <thearnabsaha201@gmail.com>
2025-07-30 23:27:41 +0530

    removed excess comments

--- apps/web/components/app-sidebar.tsx
+++ apps/web/components/app-sidebar.tsx
@@ -1,27 +1,3 @@
-// import {
-//     Sidebar,
-//     SidebarContent,
-//     SidebarFooter,
-//     SidebarGroup,
-//     SidebarHeader,
-// } from "@workspace/ui/components/sidebar"
-// import {
-//   ResizableHandle,
-//   ResizablePanel,
-//   ResizablePanelGroup,
-// } from "@workspace/ui/components/resizable"
-// export function AppSidebar() {
-//     return (
-//         <Sidebar>
-//             <SidebarHeader />
-//             <SidebarContent>
-//                 <SidebarGroup />
-//                 <SidebarGroup />
-//             </SidebarContent>
-//             <SidebarFooter />
-//         </Sidebar>
-//     )
-// }
 "use client";
 
 import * as React from "react";
