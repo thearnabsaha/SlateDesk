8eb1b7ad3e9808ad7ce6e0c398165e3ae254166d
thearnabsaha <thearnabsaha201@gmail.com>
2025-08-05 00:03:10 +0530

    feat: add popover for additional actions in DocumentItem component

--- apps/web/components/DocumentItem.tsx
+++ apps/web/components/DocumentItem.tsx
@@ -1,4 +1,5 @@
-import { ChevronDownIcon, ChevronUpIcon, Ellipsis, LucideIcon, Plus } from 'lucide-react'
+import { Popover, PopoverContent, PopoverTrigger } from '@workspace/ui/components/popover'
+import { ChevronDownIcon, ChevronUpIcon, Ellipsis, LucideIcon, Plus, Trash } from 'lucide-react'
 import React, { useEffect, useState } from 'react'
 interface ItemProps {
     label?: string
@@ -10,6 +11,7 @@ let paddingleft = 5;
 const DocumentItem = ({ label, onClick, icon: Icon, children }: ItemProps) => {
     const [open, setOpen] = useState(false)
     const [childitem, setChilditem] = useState(1)
+    const [opacity, setOpacity] = useState(false)
     useEffect(() => {
         if (children) {
             setChilditem((prev) => prev + 1)
@@ -18,13 +20,22 @@ const DocumentItem = ({ label, onClick, icon: Icon, children }: ItemProps) => {
 
     return (
         <div>
-            <div className='flex w-full items-center text-muted-foreground font-[500] py-1 hover:bg-accent cursor-pointer' onClick={() => setOpen((prev) => !prev)}>
+            <div className='group/document-item flex w-full items-center text-muted-foreground font-[500] py-1 hover:bg-accent cursor-pointer' onClick={() => setOpen((prev) => !prev)}>
                 {open ? <ChevronDownIcon className='size-4 ml-4 mr-2' /> :
                     <ChevronUpIcon className='size-4 ml-4 mr-2' />}
                 <Icon className='mr-2 size-4' />
                 <p className=''>{label}</p>
-                {<div className='ml-auto flex'>
-                    <Ellipsis className='mr-3 size-4' />
+                {<div className='ml-auto flex invisible group-hover/document-item:visible' onClick={(e) => e.stopPropagation()}>
+                    <Popover>
+                        <PopoverTrigger asChild className='cursor-pointer'>
+                            <Ellipsis className='mr-3 size-4' />
+                        </PopoverTrigger>
+                        <PopoverContent className="m-0 p-0 w-56 translate-x-30 bg-accent">
+                            <h1 className="flex items-center p-2 cursor-pointer hover:bg-background" onClick={(e) => e.stopPropagation()}><Trash className='mr-3 size-4' />Delete</h1>
+                            <div className='bg-ring w-full h-[1px]' />
+                            <h1 className="p-2 text-xs font-[500] text-ring">Last Edited by: Arnab Saha</h1>
+                        </PopoverContent>
+                    </Popover>
                     <Plus className='mr-3 size-4' />
                 </div>}
             </div>
