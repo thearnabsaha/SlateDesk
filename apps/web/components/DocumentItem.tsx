b21264438ed9f1dc1c76cd565cbc2988639b0897
thearnabsaha <thearnabsaha201@gmail.com>
2025-08-04 23:33:01 +0530

    feat: add new icons to DocumentItem component and refactor accordion toggle logic

--- apps/web/components/DocumentItem.tsx
+++ apps/web/components/DocumentItem.tsx
@@ -1,4 +1,4 @@
-import { ChevronDownIcon, ChevronUpIcon, LucideIcon } from 'lucide-react'
+import { ChevronDownIcon, ChevronUpIcon, Ellipsis, LucideIcon, Plus } from 'lucide-react'
 import React, { useEffect, useState } from 'react'
 interface ItemProps {
     label?: string
@@ -10,9 +10,6 @@ let paddingleft = 5;
 const DocumentItem = ({ label, onClick, icon: Icon, children }: ItemProps) => {
     const [open, setOpen] = useState(false)
     const [childitem, setChilditem] = useState(1)
-    const handleAccordion = () => {
-        setOpen((prev) => !prev)
-    }
     useEffect(() => {
         if (children) {
             setChilditem((prev) => prev + 1)
@@ -21,11 +18,15 @@ const DocumentItem = ({ label, onClick, icon: Icon, children }: ItemProps) => {
 
     return (
         <div>
-            <div className='flex w-full items-center text-muted-foreground font-[500] py-1 hover:bg-accent cursor-pointer' onClick={handleAccordion}>
+            <div className='flex w-full items-center text-muted-foreground font-[500] py-1 hover:bg-accent cursor-pointer' onClick={() => setOpen((prev) => !prev)}>
                 {open ? <ChevronDownIcon className='size-4 ml-4 mr-2' /> :
                     <ChevronUpIcon className='size-4 ml-4 mr-2' />}
                 <Icon className='mr-2 size-4' />
                 <p className=''>{label}</p>
+                {<div className='ml-auto flex'>
+                    <Ellipsis className='mr-3 size-4' />
+                    <Plus className='mr-3 size-4' />
+                </div>}
             </div>
             {open && <div style={{ paddingLeft: (paddingleft * childitem) + "px" }}>{children ? children : <p className='font-[500] text-sm text-ring' style={{ paddingLeft: (paddingleft + 30 * childitem) + "px" }}>No pages Inside</p>}</div>}
         </div>
